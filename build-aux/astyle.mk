3a9fdc5ae7461d9c476792006f14069475023ac2
Jamal Natour <Jamal.Natour@fumasoftware.com>
2017-09-12 01:48:25 +0100

    Patch: remove old build-aux

--- build-aux/astyle.mk
+++
@@ -1,7 +0,0 @@
-if HAVE_ASTYLE
-astyle:
-	$(ASTYLE) -n -R "$(top_srcdir)/sources/*.hpp"
-	$(ASTYLE) -n -R "$(top_srcdir)/sources/*.cpp"
-	$(ASTYLE) -n -R "$(top_srcdir)/tests/*.hpp"
-	$(ASTYLE) -n -R "$(top_srcdir)/tests/*.cpp"
-endif # HAVE_ASTYLE
